c472ff603811ca066aeadb349e2ec36142ec6374
udayg24 <udaygajera116@gmail.com>
2022-11-04 19:11:02 +0530

    sec commit

--- packages/react-app/pages/_app.tsx
+++ packages/react-app/pages/_app.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import '@celo/react-celo/lib/styles.css';
+import "@celo/react-celo/lib/styles.css";
 import { SnackbarProvider } from "notistack";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/apollo-client";
 import { Alfajores, CeloProvider } from "@celo/react-celo";
 import { AppProps } from "next/app";
 import { CustomThemeProvider } from "@/contexts/userTheme";
-import { Provider } from "react-redux"
-import store from "@/state/index"
-import AppUpdater from "@/state/app/updater"
+import { Provider } from "react-redux";
+import store from "@/state/index";
+import AppUpdater from "@/state/app/updater";
 
 function Updaters() {
   return (
     <>
       <AppUpdater />
     </>
-  )
+  );
 }
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
@@ -24,10 +24,10 @@ function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
       <CustomThemeProvider>
         <CeloProvider
           dapp={{
-            name: "Celo Composer",
-            description: "A demo dApp to showcase functionality",
+            name: "Celo demo app",
+            description: "a celo demo app",
             url: "https://celo-composer.netlify.app/",
-            icon: "https://celo-composer.netlify.app/favicon.ico",
+            icon: "",
           }}
           network={Alfajores}
           // networks={[Mainnet, Alfajores]}
@@ -39,12 +39,10 @@ function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
               horizontal: "right",
             }}
           >
-            <Updaters/>
+            <Updaters />
             <ApolloProvider client={client}>
               <div suppressHydrationWarning>
-                {typeof window === "undefined" ? null : (
-                  <Component {...pageProps} />
-                )}
+                {typeof window === "undefined" ? null : <Component {...pageProps} />}
               </div>
             </ApolloProvider>
           </SnackbarProvider>
